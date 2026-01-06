279a572924bf5c1b13eaca55267987f9e1d07776
ncmo-cyber <nonocuomo@gmail.com>
2026-01-06 17:55:38 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 const employees = [
-  // Cube Golem France
   {id:1, firstName:"Alexis", lastName:"DESPLATS", role:"CEO", company:"Cube Golem France", managerId:null, email:"a.desplats@saga-alliance.com", phone:"+33 6 47 32 13 75"},
   {id:2, firstName:"Fabien", lastName:"SOUTIF", role:"Logistics Director", company:"Cube Golem France", managerId:1, email:"f.soutif@khadou.fr", phone:"+33 1 30 47 47 29"},
   {id:3, firstName:"Adrien", lastName:"REAUX", role:"Sales Director", company:"Cube Golem France", managerId:1, email:"a.reaux@cubegolem.fr", phone:"+33 1 30 47 47 28"},
@@ -9,23 +8,23 @@ const employees = [
   {id:7, firstName:"Noémie", lastName:"CUOMO", role:"Supply Chain & Logistic Assistant", company:"Cube Golem France", managerId:2, email:"n.cuomo@khadou.fr", phone:"+33 1 30 47 21 62"},
   {id:8, firstName:"Matthieu", lastName:"BONNINGUE", role:"Forklift Operator", company:"Cube Golem France", managerId:2, email:"m.bonningue@cubegolem.fr", phone:"+33 1 30 47 47 31"},
   {id:9, firstName:"Thibaud", lastName:"HERPIN", role:"Stock Manager", company:"Cube Golem France", managerId:2, email:"logistique@cubegolem.fr", phone:"+33 1 30 47 21 60"},
-
-  // Cube Golem Germany
-  {id:15, firstName:"Reinhard", lastName:"SIGEL", role:"CO-CEO", company:"Cube Golem Germany", managerId:10, email:"reinhard.sigel@funtainment.de", phone:"0049 89 614219 32"},
+  {id:10, firstName:"Alexander", lastName:"DUBYNSKI", role:"Managing Partner", company:"Cube Golem Group", managerId:null, email:"a.dubynski@saga-alliance.com", phone:"+49 177 300 0000"},
+  {id:11, firstName:"Gregory", lastName:"BENASSAR", role:"CEO", company:"Heathside", managerId:null, email:"greg@heathside.biz", phone:"+33 6 32 12 54 60"},
+  {id:12, firstName:"Alistair", lastName:"RINGLAND", role:"Logistic Director", company:"Heathside", managerId:11, email:"alistair@heathside.biz", phone:"+44 777 190 00240"},
+  {id:13, firstName:"Nic", lastName:"ALDRIDGE", role:"Head of Khadou Global", company:"Heathside", managerId:11, email:"nic@khadou.com", phone:"+44 794 782 5133"},
+  {id:14, firstName:"Sam", lastName:"MYERS", role:"Head of Product Management", company:"Heathside", managerId:11, email:"sam@heathside.biz", phone:"+44 753 491 4120"},
+  {id:15, firstName:"Reinhard", lastName:"SIGEL", role:"CO CEO", company:"Cube Golem Germany", managerId:10, email:"reinhard.sigel@funtainment.de", phone:"0049 89 614219 32"},
   {id:16, firstName:"Jurgen", lastName:"BROY", role:"CO-CEO", company:"Cube Golem Germany", managerId:10, email:"", phone:""},
   {id:17, firstName:"Peter", lastName:"MÜLLER", role:"Logistics Director", company:"Cube Golem Germany", managerId:15, email:"peter.mueller@funtainment.de", phone:"0049 89 614219 22"},
   {id:18, firstName:"", lastName:"", role:"Sales Director", company:"Cube Golem Germany", managerId:15, email:"", phone:""},
   {id:19, firstName:"Daniel", lastName:"KRÜCKEL", role:"Purchase Director", company:"Cube Golem Germany", managerId:15, email:"daniel.krueckel@funtainment.de", phone:"0049 89 614219 26"},
   {id:20, firstName:"Andrea", lastName:"KLÜGER", role:"Head of Bookkeeping", company:"Cube Golem Germany", managerId:15, email:"andrea.klueger@funtainment.de", phone:"0049 89 614219 24"},
-
-  // Cube Golem Slovenia
+  {id:21, firstName:"Arnaud", lastName:"FRANGEUL", role:"CO-CEO", company:"Hubtoys", managerId:null, email:"a.frangeul@hubtoys.fr", phone:"+33 6 75 25 32 60"},
+  {id:22, firstName:"José", lastName:"MARTINEZ", role:"CO-CEO", company:"Hubtoys", managerId:null, email:"j.martinez@hubtoys.fr", phone:""},
+  {id:23, firstName:"Thomas", lastName:"TURBE", role:"", company:"Hubtoys", managerId:21, email:"t.turbe@hubtoys.fr", phone:""},
+  {id:24, firstName:"Eve", lastName:"SOULLIER", role:"Administrative Assistant", company:"Hubtoys", managerId:21, email:"support@hubtoys.fr", phone:"+33 6 89 93 33 69"},
   {id:25, firstName:"Nik", lastName:"KROSELJ", role:"CEO", company:"Cube Golem Slovenia", managerId:null, email:"n.kroselj@cubegolem.si", phone:"+38 6 51 24 04 40"},
   {id:26, firstName:"Adja", lastName:"SOSKO", role:"Warehouse Manager", company:"Cube Golem Slovenia", managerId:25, email:"a.sosko@cubegolem.si", phone:"+38 6 40 75 25 17"},
-
-  // Cube Golem Group
-  {id:10, firstName:"Alexander", lastName:"DUBYNSKI", role:"Managing Partner", company:"Cube Golem Group", managerId:null, email:"a.dubynski@saga_alliance.com", phone:"+49 177 300 0000"},
-
-  // Khadou France
   {id:27, firstName:"Alexis", lastName:"DESPLATS", role:"CEO", company:"Khadou France", managerId:null, email:"a.desplats@saga-alliance.com", phone:"+33 6 47 32 13 75"},
   {id:28, firstName:"Fabien", lastName:"SOUTIF", role:"Logistics Director", company:"Khadou France", managerId:27, email:"f.soutif@khadou.fr", phone:"+33 1 30 47 47 29"},
   {id:29, firstName:"Adrien", lastName:"REAUX", role:"Sales & Purchases Director", company:"Khadou France", managerId:27, email:"a.reaux@cubegolem.fr", phone:"+33 1 30 47 47 28"},
@@ -34,8 +33,5 @@ const employees = [
   {id:32, firstName:"Doaa", lastName:"KADDOURI", role:"Sales & Purchase Assistant", company:"Khadou France", managerId:29, email:"d.kaddouri@khadou.fr", phone:"+33 1 30 47 21 61"},
   {id:33, firstName:"Noémie", lastName:"CUOMO", role:"Supply Chain & Logistic Assistant", company:"Khadou France", managerId:28, email:"n.cuomo@khadou.fr", phone:"+33 1 30 47 21 62"},
   {id:34, firstName:"Matthieu", lastName:"BONNINGUE", role:"Forklift Operator", company:"Khadou France", managerId:28, email:"m.bonningue@cubegolem.fr", phone:"+33 1 30 47 47 31"},
-  {id:35, firstName:"Thibaud", lastName:"HERPIN", role:"Stock Manager", company:"Khadou France", managerId:28, email:"logistique@cubegolem.fr", phone:"+33 1 30 47 21 60"},
-
-  // Heathside
-  {id:11, firstName:"Gregory", lastName:"BENASSAR", role:"CEO", company:"Heathside", managerId:null, email:"greg@heathside.biz", phone:"+33 6 32 12 54 60"},
-  {id:12, firstName:"Alistair
+  {id:35, firstName:"Thibaud", lastName:"HERPIN", role:"Stock Manager", company:"Khadou France", managerId:28, email:"logistique@cubegolem.fr", phone:"+33 1 30 47 21 60"}
+];
